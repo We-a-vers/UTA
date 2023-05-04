c2aad3fdba43f1da979f287960eb43c8289230fa
Yosen Lin <104092281+y00628@users.noreply.github.com>
2023-05-03 18:42:41 -0700

    Add files via upload

---
+++ source/scripts/protected.js
@@ -0,0 +1,39 @@
+
+const logOutBtn = document.querySelector('.log-out');
+const modal = document.querySelector('.modal');
+const closeBtn = document.querySelector('.close');
+const form = document.querySelector('form');
+
+
+// Close the modal and reset the form
+function closeModal(){
+    modal.style.display = 'none';
+    form.reset();
+}
+
+// Display the modal
+logOutBtn.addEventListener('click', ()=>{
+    modal.style.display = 'block';
+})
+
+// Jump to the protected page if passcode matches
+logOutBtn.addEventListener('click', ()=>{
+    console.log("You have successfully logged out");
+    form.reset();
+    window.location.href = "./source/index.html";
+})
+
+// Close by clicking outside of the form
+modal.addEventListener('click', (e)=>{
+    if(e.target === modal){
+        closeModal();
+    }
+})
+
+// Close by pressing Escape Key
+document.addEventListener('keydown', (e)=>{
+    if(e.key === "Escape" && modal.style.display === 'block'){
+        closeModal();
+    }
+})
+
