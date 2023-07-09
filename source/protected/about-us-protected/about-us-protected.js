1bbeb83c1ea774e094f3a9ba095749c75042b5e0
DavidWangwjh <davidwang.wjh@gmail.com>
2023-07-08 18:59:52 -0700

    styled form

--- source/protected/about-us-protected/about-us-protected.js
+++ source/protected/about-us-protected/about-us-protected.js
@@ -1,7 +1,16 @@
 const pencilButton = document.getElementById("pencilButton");
 const popupForm = document.getElementById("popupForm");
+const formCancelButton = document.getElementById("form-cancel-button");
+const formCheckMark = document.getElementById("form-check-mark");
 
 pencilButton.addEventListener("click", () => {
     popupForm.style.display = "block";
 });
 
+formCancelButton.addEventListener("click", () => {
+    popupForm.style.display = "none";
+});
+
+formCheckMark.addEventListener("click", () => {
+    popupForm.style.display = "none";
+});
