352303de091630474a6c89a55ac6a31ea358789b
y00628 <yol043@ucsd.edu>
2023-07-08 18:36:43 -0700

    about us

---
+++ source/protected/about-us-protected/about-us-protected.js
@@ -0,0 +1,7 @@
+const pencilButton = document.getElementById("pencilButton");
+const popupForm = document.getElementById("popupForm");
+
+pencilButton.addEventListener("click", () => {
+    popupForm.style.display = "block";
+});
+
