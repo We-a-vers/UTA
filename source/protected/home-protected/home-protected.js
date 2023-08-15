d63e06b369396a378836da5b3895ad0b80642eed
y00628 <yol043@ucsd.edu>
2023-08-15 23:06:11 +0800

    removed popup modules

--- source/protected/home-protected/home-protected.js
+++ source/protected/home-protected/home-protected.js
@@ -1,20 +1,9 @@
-const openModal = document.querySelector('#plus');
-const closeModal = document.querySelector('#close-btn');
-const modal = document.querySelector('.modal');
+
 const memberImageUploadBtn = document.getElementById("member-image-upload");
 const memberImageFile = document.getElementById("member-image-file");
 const memberImage = document.getElementById("member-input-image");
 
 
-
-openModal.addEventListener('click', () => {
-    modal.showModal();
-})
-
-
-closeModal.addEventListener('click', () => {
-    modal.close();
-})
 
 
 memberImageUploadBtn.addEventListener("click", function() {
