b5955d9981a5a1fdb473aa47a80a276cabffeb77
DavidWangwjh <davidwang.wjh@gmail.com>
2023-09-07 20:03:13 -0700

    some stuff

--- source/sponsors/sponsors.js
+++ source/sponsors/sponsors.js
@@ -47,3 +47,82 @@ window.addEventListener("load", async () => {
         merchPurchaseButton.setAttribute("onclick", "window.location.href='" + merch.link + "';");
     }
 });
+
+
+// <div class="uta-sponsors__logos">
+//         <div class="uta-sponsors__logo-wrapper">
+//             <img src="Background.png" alt="Sponsor">
+//             <div class="uta-sponsors__discount">10% off</div>
+//             <div class="tooltiptext">
+//                 <div class="tooltiptext-title">Meet Fresh</div>
+
+//                 <div class="tooltiptext-body">
+//                     <div class="tooltiptext-discount">Discount: 10% Off</div>
+//                     <ul class="tooltiptext-content">
+//                         <li>Meet Fresh is THE BEST Taiwanese dessert place ever! They are best known for their hand-made dessert such as icy grass jelly and many more!</li>
+//                     </ul>
+//                 </div>
+//             </div>
+//         </div>
+
+const sponsorsContainer = document.querySelector('.uta-sponsors__logos');
+// helper function that creates the corresponding html elements and append them to the section defined above
+async function addSponsorToHtml(sponsorId, sponsorName, sponsorBenefitInfo) {
+    try {
+        // get storage reference with path
+        const sRef = storageRef(storage, `sponsors/current/${sponsorId}`);
+        const url = await getDownloadURL(sRef);
+    
+        // create element, add class, add style
+        let sponsor = sponsorsContainer.querySelector(`#${sponsorId}`)
+
+        if (!sponsor) {
+            sponsor = document.createElement("div");
+            sponsor.classList.add('uta-sponsors__logo-wrapper');
+            sponsor.id = sponsorId;
+
+            sponsorsContainer.appendChild(sponsor);
+
+            const sponsorImage = document.createElement("img");
+            sponsorImage.src = url;
+            sponsorImage.alt = "Sponsor Image";
+
+            const sponsorBenefitInfo = document.createElement("div");
+            sponsorBenefitInfo.classList.add('uta-sponsors__discount');
+        }
+        
+    } catch (error) {
+        console.error("Error loading member picture:", error);
+    }
+}
+
+async function reloadData() {
+    // create database reference
+    const dbRef = ref(database, 'boardMembers/members');
+    const snapshot = await get(dbRef);
+  
+    if (snapshot.exists()) {
+        // retrieve data
+        const membersArray = [];
+
+        snapshot.forEach((positionSnap) => {
+            const membersInfo = positionSnap.val();
+            // sort data by created date
+            for (const memberId in membersInfo) {
+                const member = membersInfo[memberId];
+                member.createdAt = new Date(member.createdAt)
+                membersArray.push(member);
+            }
+        })
+
+        membersArray.sort((a, b) => new Date(a.createdAt) - new Date(b.createdAt));
+
+        // iterate through each data and call the helper function
+        for (const member of membersArray) {
+            await addMemberToHtml(member.id, member.role, member.name);
+        }
+    }
+}
+
+// window load listener
+document.addEventListener("DOMContentLoaded", reloadData);
