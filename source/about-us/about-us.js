e23eba7fc5afc356bda1e32ef977f65b93a86019
Joanne <jpon@ucsd.edu>
2023-09-08 18:34:09 -0700

    update

--- source/about-us/about-us.js
+++ source/about-us/about-us.js
@@ -1,12 +1,9 @@
 import { firebase, database, ref, set, get, push, remove, storage, storageRef, uploadBytes, getDownloadURL, deleteObject } from '../firebase/firebase.js';
 
-const memberImage = document.querySelector('#header-image');
-const headerText = document.querySelector('#headerText');
-
 // window load listener
 window.addEventListener("load", async () => {
     // create database reference
-    const dbRef = ref(database, 'aboutUs/memberHeader');
+    const dbRef = ref(database, 'AboutUs/memberHeader');
     const snapshot = await get(dbRef);
   
     if (snapshot.exists()) {
@@ -16,25 +13,22 @@ window.addEventListener("load", async () => {
         const sRef = storageRef(storage, "memberHeaderPicture.png");
         const imageUrl = await getDownloadURL(sRef);
 
-        mamberImage.src = imageUrl;
-        memberImage.alt = 'About Us Member Image';
-        headerText.textContent = memberHeader.description;
-
-        const signUpButton = document.createElement('button');
-        const link = document.createElement('a');
-
-        if(MembersHeader.url != ""){
-            signUpButton.textContent = "Join our intern program";
-            signUpButton.className = 'link';
-
-            link.href = boardMembersHeader.url;
-        }
-        else{
-            signUpButton.textContent = "No Link Provided";
-            signUpButton.className = 'no-link';
-        }
-
-        link.appendChild(signUpButton);
-        headerText.insertAdjacentElement('afterend',link);
+        memberImage.src = imageUrl;
+        member.alt = 'About Us Member Image';
+        descriptionInput.value = memberHeader.description;
+        urlInput.value = memberHeader.url;
     }
 });
+
+// open the modal by clicking the plus icon
+openModal.addEventListener('click', () => {
+    modal.showModal();
+})
+
+// close the modal by clicking the cancel button
+closeModal.addEventListener('click', () => {
+    modalForm.reset();
+    modalImage.src = "/source/assets/placeholder-image.png";
+    modal.close();
+})
+
